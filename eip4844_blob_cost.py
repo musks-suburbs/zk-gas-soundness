1c7b0d7456ceabd09b929482a1259d16e2b6d9fc
Leo <152180094+musks-suburbs@users.noreply.github.com>
2025-11-15 14:09:20 +0700

    Add more known networks to NETWORKS
    Better labels for popular L2s

--- eip4844_blob_cost.py
+++ eip4844_blob_cost.py
@@ -35,6 +35,9 @@ NETWORKS = {
     10: "Optimism",
     137: "Polygon",
     42161: "Arbitrum One",
+    8453: "Base",
+    59144: "Linea",
+    324: "zkSync Era",
 }
 
 def network_name(cid: int) -> str:
